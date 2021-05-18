bbef4b912c17834d2c92eee61704817f7fcc30a7
Seiichi Ariga <seiichi.ariga@gmail.com>
2021-05-19 07:50:57 +0900

    例

---
+++ src/weathercheck/common/private-example.nim
@@ -0,0 +1,7 @@
+# Copyright (c) 2021 Seiichi Ariga <seiichi.ariga@gmail.com>
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+let
+  OPEN_WEATHER_API_KEY* = "EXAMPLE_API_KEY"
